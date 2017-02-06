cdd2b9649de031247e01ffc624c1960a59931a2b
邵羽 <264115809@qq.com>
2017-02-06 19:59:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ apt-get update && apt-get install -y vim python-pip supervisor
 pip install shadowsocks
 
 # clone the shadowsocks configuration
-wget --no-check-certificate https://github.com/sy264115809/ss-init/blob/master/shadowsocks.json -P /etc
+wget --no-check-certificate https://raw.githubusercontent.com/sy264115809/ss-init/master/shadowsocks.json -P /etc
 
 # clone the supervisor config
-wget --no-check-certificate https://github.com/sy264115809/ss-init/blob/master/supervisor.conf -O shadowsocks.conf -P /etc/supervisor/conf.d
+wget --no-check-certificate https://raw.githubusercontent.com/sy264115809/ss-init/master/supervisor.conf -O shadowsocks.conf -P /etc/supervisor/conf.d
 
 # config iptables
-wget --no-check-certificate https://github.com/sy264115809/ss-init/blob/master/iptables.rules -O iptables.test.conf -P /etc
+wget --no-check-certificate https://raw.githubusercontent.com/sy264115809/ss-init/master/iptables.rules -O iptables.test.conf -P /etc
 iptables-restore < /etc/iptables.test.rules
 iptables -L
 iptables-save > /etc/iptables.up.rules
@@ -20,5 +20,5 @@ chmod +x /etc/network/if-pre-up.d/iptables
 
 # install fail2ban
 apt-get install fail2ban  
-wget --no-check-certificate https://github.com/sy264115809/ss-init/blob/master/jail.local -P /etc/fail2ban
+wget --no-check-certificate https://raw.githubusercontent.com/sy264115809/ss-init/master/jail.local -P /etc/fail2ban
 service fail2ban restart 
